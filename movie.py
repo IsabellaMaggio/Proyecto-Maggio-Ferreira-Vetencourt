28dc7bd7056785396400cc3d846c3f69e88102fd
Isabella Ferreira <isabella.ferreira@correo.unimet.com>
2024-08-06 21:35:19 -0400

    creacion clase movie

---
+++ movie.py
@@ -0,0 +1,10 @@
+class Movie():
+
+    def __init__(self, title, episode_id, release_date, director, opening_crawl, characters):
+        self.title = title
+        self.episode_id = episode_id
+        self.release_date = release_date
+        self.director = director
+        self.opening_crawl = opening_crawl
+        self.characters = characters
+                      
