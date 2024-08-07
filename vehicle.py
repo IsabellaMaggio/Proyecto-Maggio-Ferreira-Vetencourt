863cef58fd721a1feddc0d911ce68bfca6a31e12
Anabella Vetencourt <anabella.vetencourt@correo.unimet.edu.ve>
2024-08-06 21:47:34 -0400

    Creacion clase Vehicle

---
+++ vehicle.py
@@ -0,0 +1,9 @@
+class Vehicle():
+    def __init__(self, name, model, manufacturer, passengers, pilots, url):
+        self.name = name
+        self.model = model
+        self.manufacturer = manufacturer
+        self.passengers = passengers
+        self.pilots = pilots
+        self.url = url
+        
