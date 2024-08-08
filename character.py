7aae2671019b00e61ed4cf9a20c66991b9f31ae9
Isabella Maggio <isabella.maggio@correo.unimet.edu.ve>
2024-08-07 22:15:37 -0400

    Creacion clase character

---
+++ character.py
@@ -0,0 +1,10 @@
+class Character():
+    def __init__(self, name, homeworld, gender, species, films, starships, vehicles, url):
+        self.name = name
+        self.homeworld = homeworld
+        self.gender = gender
+        self.species = species
+        self.films = films
+        self.starships = starships
+        self.vehicles = vehicles
+        self.url = url
