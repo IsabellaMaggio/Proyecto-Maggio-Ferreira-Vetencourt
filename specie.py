005881b144dec1f888da793aac096b0b88e05e73
Isabella Maggio <isabella.maggio@correo.unimet.edu.ve>
2024-08-07 22:16:40 -0400

    Creacion clase specie

---
+++ specie.py
@@ -0,0 +1,10 @@
+class Specie():
+    def __init__(self, name, average_height, classification, homeworld, language, people, films, url):
+        self.name = name
+        self.average_height = average_height
+        self.classification = classification
+        self.homeworld = homeworld
+        self.language = language
+        self.people = people
+        self.films = films
+        self.url = url
